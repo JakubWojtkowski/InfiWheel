4dfa54cd165fc8aea5b527276abd228cdba20e70
wojtko <j.w2001@onet.pl>
2023-11-10 15:37:47 +0100

    Add social buttons. Improve Login appearance.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,4 @@
+import { Apple, FacebookSharp, Google } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
@@ -9,15 +10,31 @@ function Login() {
           <LoginForm>
             <Heading>Welcome back!</Heading>
             <SubHeading>Please enter your details</SubHeading>
-            <Input type="text" placeholder="Email"></Input>
-            <Input type="password" placeholder="Password"></Input>
-            <PasswdForgot>Forgot Password?</PasswdForgot>
-
-            <LoginBtn>Log In</LoginBtn>
-            <Register>
+            <Input type="text" placeholder="Email" name="email"></Input>
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+            ></Input>
+            <OptionalText password>Forgot Password?</OptionalText>
+            <LoginBtn>Login</LoginBtn>
+            <OptionalText>or continue with</OptionalText>
+            <LoginBtns>
+              <SecondaryLoginBtn>
+                <Google />
+              </SecondaryLoginBtn>
+              <SecondaryLoginBtn>
+                <Apple />
+              </SecondaryLoginBtn>
+              <SecondaryLoginBtn>
+                <FacebookSharp />
+              </SecondaryLoginBtn>
+            </LoginBtns>
+            <OptionalText>
               Don't have an account? <strong>Sign Up</strong>
-            </Register>
+            </OptionalText>
           </LoginForm>
+
           <LoginImage>
             <Image
               src="https://i.insider.com/60dd87bfcad1220011caf684?format=jpeg"
@@ -45,7 +62,7 @@ const Wrapper = styled.div`
 
 const Main = styled.div`
   display: grid;
-  grid-template-columns: 0.45fr 0.55fr;
+  grid-template-columns: 0.4fr 0.55fr;
   place-items: center;
 
   @media only screen and (max-width: 600px) {
@@ -60,9 +77,7 @@ const LoginForm = styled.form`
   gap: 24px;
 `;
 
-const LoginImage = styled.div`
-  flex: 0.75;
-`;
+const LoginImage = styled.div``;
 
 const Image = styled.img`
   width: 100%;
@@ -73,6 +88,7 @@ const Image = styled.img`
 const Heading = styled.h2`
   font-size: 2.5rem;
   letter-spacing: 1px;
+  text-align: center;
 `;
 
 const SubHeading = styled.span`
@@ -83,12 +99,14 @@ const SubHeading = styled.span`
 
 const Input = styled.input`
   font-size: 1rem;
+  width: 320px;
   background: #e5e5e5;
   border: none;
   border-bottom: 1px solid #202020;
   padding: 0 48px 16px 0;
-  transition: all 250ms ease-in-out;
+  transition: padding 250ms ease-in-out;
   color: #202020;
+  opacity: 0.55;
 
   &::placeholder {
     color: #202020;
@@ -97,7 +115,7 @@ const Input = styled.input`
   &:focus {
     outline: none;
     padding: 0 48px 10px 0;
-    border-bottom: 1px solid #202020;
+    opacity: 1;
   }
 `;
 
@@ -107,18 +125,37 @@ const LoginBtn = styled.button`
   padding: 16px 32px;
   border: none;
   border-radius: 32px;
+  font-weight: bold;
+  letter-spacing: 0.25px;
+  color: #202020;
+`;
+
+const LoginBtns = styled.div`
+  display: flex;
+  gap: 24px;
+  justify-content: center;
   margin-bottom: 48px;
 `;
 
-const PasswdForgot = styled.span`
-  font-size: 0.85rem;
-  opacity: 0.65;
+const SecondaryLoginBtn = styled.button`
+  width: 48px;
+  height: 48px;
+  background: transparent;
+  border: none;
   cursor: pointer;
-  text-align: right;
-  margin-bottom: 24px;
+  background: #fca311;
+  border-radius: 50%;
+  display: grid;
+  place-items: center;
+  color: #202020;
+
+  .MuiSvgIcon-root {
+    font-size: 24px !important;
+  }
 `;
 
-const Register = styled.span`
-  font-size: 0.85rem;
-  text-align: center;
+const OptionalText = styled.span`
+  font-size: 0.8rem;
+  cursor: pointer;
+  text-align: ${(props) => (props.password ? "right" : "center")};
 `;
